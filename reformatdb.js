9b3ca04f1121e691b7bcb577bf6efc582e4c8a0c
Aurora Silva <aurorafsilva1@gmail.com>
2023-04-03 20:32:59 -0300

    Refatoração do banco de dados

--- reformatdb.js
+++ reformatdb.js
@@ -1,9 +1,14 @@
 const fs = require('fs'); 
 const receitas = require('./receitas.json');
 const teste = [receitas[0], receitas[1], receitas[2]];
-const reformatacao = teste.map(receita => {
-    return receita
+const reformatacao = teste.map(receita => {     
+    return {
+        "Nome": receita.nome,
+        "Ingredientes": receita.secao[0].conteudo,
+        "Modo de preparo": receita.secao[1].conteudo, 
+    }
 })
+
 console.log(reformatacao);
 
 
