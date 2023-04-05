3a7594a94dc15b9c2ae77d918cd5feaf6e4dec13
Aurora Silva <aurorafsilva1@gmail.com>
2023-04-05 12:33:33 -0300

    Add the reformatted database

--- reformatdb.js
+++ reformatdb.js
@@ -1,35 +1,17 @@
 const fs = require('fs'); 
 const receitas = require('./receitas.json');
 const teste = [receitas[0], receitas[1], receitas[2]];
-const reformatacao = teste.map(receita => {     
-    return {
-        "Nome": receita.nome,
-        "Ingredientes": receita.secao[0].conteudo,
-        "Modo de preparo": receita.secao[1].conteudo, 
-    }
-})
+const reformat = receitas.map(receita => {
+    const newFormat = {
+        "nome": receita.nome,
+        "secao": receita.secao,
+    };
+    return newFormat;
 
-console.log(reformatacao);
-
+});
 
 
+const reformatString = JSON.stringify(reformat, null, 1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const receitasString = JSON.stringify(receitas, null, 1);
-
-fs.writeFile("./receitas.json", receitasString, error => console.log(error))*/
+fs.writeFile("./recipes.json", reformatString, error => console.log(error))
 
